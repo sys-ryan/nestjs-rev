1740a5afe4f78266c164293997adeedecff46ec0
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-03-15 15:57:27 +0900

    test: users controller
    - findAllUsers returns a list of users with the given email.
    - findUser returns a user with a given id.
    - findfUser throws an error if user with given id is not found
    - signin updates session object and returns user

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -15,7 +15,7 @@ import {
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
-import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 
@@ -23,7 +23,7 @@ import { CurrentUser } from './decorators/current-user.decorator';
 // import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 import { User } from './user.entity';
 
-import { AuthGuard } from 'src/guards/auth.guard';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)

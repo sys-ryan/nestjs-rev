1d60e392e09d389cd86cd83a5b300a92bafd2c5e
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-03-18 21:02:39 +0900

    feat: report approval

--- mycv/src/reports/reports.controller.ts
+++ mycv/src/reports/reports.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { CurrentUser } from '../users/decorators/current-user.decorator';
 import { User } from '../users/user.entity';
 import { AuthGuard } from '../guards/auth.guard';
@@ -6,6 +13,7 @@ import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
+import { ApprovedReportDto } from './dtos/approved-report.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -16,5 +24,10 @@ export class ReportsController {
   @Serialize(ReportDto)
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
     return this.reportsService.create(body, user);
+  }
+
+  @Patch('/:id')
+  approvedReport(@Param('id') id: string, @Body() body: ApprovedReportDto) {
+    return this.reportsService.changeApproval(id, body.approved);
   }
 }
